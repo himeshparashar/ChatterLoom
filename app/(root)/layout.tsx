a96443cf5e5cbf7168212e08bcfa5d1a560e771d
Himesh Parashar <59964516+himeshparashar@users.noreply.github.com>
2024-01-03 06:01:10 -0800

    File name casing updated

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,10 +5,10 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
 import "../globals.css";
-import LeftSidebar from "@/components/shared/LeftSidebar";
-import Bottombar from "@/components/shared/Bottombar";
-import RightSidebar from "@/components/shared/RightSidebar";
-import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSideBar";
+import Bottombar from "@/components/shared/BottomBar";
+import RightSidebar from "@/components/shared/RightSideBar";
+import Topbar from "@/components/shared/TopBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
